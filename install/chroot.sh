4f1c911fd909397bb684fff3080653356458a335
Mark Andreas Emmons <memmon3@uic.edu>
2017-05-21 02:00:52 -0500

    Just. Like. The fastest installation speeds

--- install/chroot.sh
+++ install/chroot.sh
@@ -43,7 +43,7 @@ install_linux(){
 	mkinitcpio -p linux
 
 	# Install and configure grub
-	pacman -S parallel wget
+	pacman -S --noconfirm parallel wget
 	PACKAGES="grub curl openssh zsh dialog wpa_actiond wpa_supplicant vim git python2 tmux"
 	pacman -Sp --noconfirm $PACKAGES | parallel wget -q -P /var/cache/pacman/pkg {}
 	pacman -S --noconfirm $PACKAGES
